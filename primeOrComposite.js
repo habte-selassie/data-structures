b15fb4199ccbd3ac5f1d82ec6e5c35d71062f669
Habte Selassie <{ID}+{username}@users.noreply.github.com>
2022-10-16 13:34:12 +0300

    intial commit

--- primeOrComposite.js
+++ primeOrComposite.js
@@ -1,18 +1,21 @@
-let num;
-let divisor = 0;
-function primeChecker(num){  
-for(i=2; i<num; i++) {
-  if(num%i === 0) {
-    divisor++;
+         
+         
+         let num;
+          let divisor = 0;
 
-    break;
-  }
-}
-if(divisor === 0)
-  console.log("is a Prime Number");
-else
-  console.log("is not a Prime Number");
+          function primeChecker(num){  
+               for(i=2; i<num; i++) {
+               if(num%i === 0) {
+                 divisor++;
+
+                }
+              }
+
+         if(divisor === 0)
+            console.log("is a Prime Number");
+            else
+             console.log("is not a Prime Number");
     
-   }
+               }
 
-primeChecker(2);
+            primeChecker(9);
